8f31a8cbc56708c687fb74f5f25c7e18736fb9ac
Zhenye Dong <dongzhenye@gmail.com>
2025-01-06 20:54:47 +0800

    refactor: initialize project structure

---
+++ address_converter/__init__.py
@@ -0,0 +1,10 @@
+from .converter import (
+    evm_to_tron,
+    tron_to_evm,
+    get_address_type,
+    normalize_evm_address,
+    validate_tron_base58_address,
+    validate_tron_hex_address
+)
+
+__version__ = "0.1.0" 
